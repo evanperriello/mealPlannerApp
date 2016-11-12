ae1dc42e71501cc83be37ea6638e885d1eda8d02
Evan Perriello <evan.perriello@gmail.com>
2016-11-12 19:06:20 +0000

    add mongodb folders and set up requires in the app file

---
+++ mealPlannerv1/app.js
@@ -0,0 +1,8 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override");
+    
