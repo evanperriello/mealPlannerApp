21af7da96dfdb06fe23e3660cea08636e53b6505
Evan Perriello <evan.perriello@gmail.com>
2016-11-27 23:25:06 +0000

    add homepage route and login form, move join script to separate file.

--- mealPlannerv1/routes/index.js
+++ mealPlannerv1/routes/index.js
@@ -16,8 +16,24 @@ router.get("/join", function(req, res){
 
 //Join SUBMITTED
 router.post("/join", function(req, res){
-    //logic to add new user to the database
-   res.render("home");
+    //include logic here to add new user to the database
+   res.redirect("home");
+});
+
+//Login FORM
+router.get("/login", function (req, res){
+    res.render("login");
+});
+
+//Login FORM
+router.post("/login", function (req, res){
+    //include logic here to verify the login credentials
+    res.redirect("home");
+});
+
+//Home Page after login/join
+router.get("/home", function(req, res){
+   res.render("home"); 
 });
 
 //export the router object to the main app file
