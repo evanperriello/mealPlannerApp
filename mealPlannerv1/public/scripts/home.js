b3292b6b968f08760a4d48dbfd0eda5d168b7fb2
Evan Perriello <evan.perriello@gmail.com>
2017-01-31 21:12:16 +0000

    delete extraneous files.

--- mealPlannerv1/public/scripts/home.js
+++
@@ -1,5 +0,0 @@
-function expandDiv(){
-    $(this).parent().children(".divToExpand").toggleClass("hiddenContent");
-    $(this).children(".glyphicon").toggleClass("glyphicon-menu-down glyphicon-menu-up");
-}
-$(".expander").click(expandDiv);
